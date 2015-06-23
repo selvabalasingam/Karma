695b5928e7f5742ffb680d52ebfe3c2e3e56ff74
Selva Balasingam <selva.balasingam@gmail.com>
2015-06-23 09:56:38 -0400

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
-$(document).ready(function(){
-	$('#nav-scroll').addClass('change color');
-	var waypoint = new Waypoint({
-	  element: document.getElementById('nav-scroll'),
-	  handler: function(direction) {},
-	  offset: 500
-	})
-}
+var color = '#FFFF99';
+var waypoint = new Waypoint({
+  element: document.querySelector('.introduction'),
+  handler: function(direction) {
+    var nav = document.querySelector('nav');
+    var background = nav.style.background;
+    if (direction === 'up') {
+      if (background === '#FFFF99') {
+        nav.style.background = 'orange';
+      }
+      else {
+        nav.style.background = '#FFFF99';
+      }
+    }
+  }
+});
+
+var imageWaypoint = new Waypoint({
+  element: document.querySelector('.introduction'),
+  handler: function(direction) {
+    var nav = document.querySelector('nav');
+    if (direction === 'down') {
+      nav.style.background = '#99FF66';
+    }
+  },
+  offset: 'bottom-in-view'
+});
